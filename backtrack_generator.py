4a2ba272c093d7c7ecf59643190c01f2bf708187
Bachir <karbac@hotmail.fr>
2023-09-22 10:44:12 +0200

    fix

--- backtrack_generator.py
+++ backtrack_generator.py
@@ -6,12 +6,12 @@ from util import *
 N = int(input("Entrez la taille du labyrinthe : "))
 #N = 100
 filename = input("Entrez le nom du labyrinthe : ")
-filename = f"random{N}"
+#filename = f"random{N}"
 start = time.time()
 maze = Maze(N,filename)
 maze.backtrack_generate()
 end = time.time()
-print(f"Running time : {end-start})
+print(f"Running time : {end-start}")
 maze.create_file()
 
 #Affiche graphiquement le labyrinthe
--- kruskal_generator.py
+++ kruskal_generator.py
@@ -9,7 +9,7 @@ start = time.time()
 maze = Maze(N,filename)
 maze.kruskal_generate()
 end = time.time()
-print(f"Running time : {end-start})
+print(f"Running time : {end-start}")
 maze.create_file()
 
 #Affiche le labyrinthe graphique
